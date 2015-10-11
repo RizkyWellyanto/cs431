79fa196e8ee9123ba4f42db8dedaa5dad89480bb
Bo Liu <boliu1@illinois.edu>
2015-10-10 20:09:02 -0500

    unit tested.

--- Debouncer/Debouncer.c
+++ Debouncer/Debouncer.c
@@ -2,33 +2,47 @@
 
 void button_init(Debouncer * button)
 {
-    button->samples[NUM_SAMPLES] = {0};
+    uint8_t i = 0;
+    for (; i < NUM_SAMPLES; ++i)
+    {
+        button->samples[NUM_SAMPLES] = 0;
+    }
+
     button->sampleIdx = 0;
-    button->debouncedValue = 0;
+    button->previousValue = 0;
     button->stable = 0;
 }
 
 void button_read(Debouncer * button, uint8_t Port_Reading)
 {
-    
+
     button->samples[button->sampleIdx] = Port_Reading;
-    
-    if (++button->sampleIdx >= NUM_SAMPLES - 1)
+
+    if (++button->sampleIdx == NUM_SAMPLES)
         button->sampleIdx = 0;
 }
 
-uint8_t button_debounce(Debouncer* button) 
+uint8_t button_debounced(Debouncer* button)
 {
     button->stable = 1;
     uint8_t i = 1;
     for(; i < NUM_SAMPLES; ++i)
     {
-        if (button->samples[0] ^ button->samples[i] == 1)
+        if ((button->samples[0] ^ button->samples[i]) == 1)
         {
             button->stable = 0;
             return UNSTABLE;
         }
     }
-    
-    return button->debouncedValue = button->samples[0];
+
+    if (button->samples[0] == button->previousValue)
+    { // value not changed: user code decides what to do
+        return UNCHANGED;
+    }
+    else // discovers a new value
+    {
+        button->previousValue = button->samples[0];
+        return button->samples[0];
+    }
 }
+
