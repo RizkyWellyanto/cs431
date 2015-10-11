e29a398bf061a68217a2cf75db29d4c8f316d6ae
Bo Liu <boliu1@illinois.edu>
2015-10-11 02:27:41 -0500

    Update flexmotor.c

--- Lab05/flexmotor.c
+++ Lab05/flexmotor.c
@@ -17,13 +17,13 @@ void motor_init(uint8_t chan)
     T2CONbits.TCKPS = 0b10; // Select 1:64 Prescaler
     CLEARBIT(IFS0bits.T2IF); // Clear Timer2 interrupt status flag
     CLEARBIT(IEC0bits.T2IE); // Disable Timer2 interrupt enable control bit
-    PR2 = CYCL; // Set timer period 20ms:
+    PR2 = PER; // Set timer period 20ms:
 
    if(chan == CHANNEL_X )
    {
-        CLEARBIT(TRISDbits.TRISD7); // Set OC8 as output 
-        OC8R = MID; // Set the initial throw to mid position
-        OC8RS = MID; // Load OCRS: next pwm duty cycle 
+        CLEARBIT(TRISDbits.TRISD7); // Set OC8 as output
+        OC8R = MID; // Set the initial to mid position
+        OC8RS = MID; // Load OCRS: next pwm duty cycle
         OC8CON = 0x0006; // Set OC8: PWM, no fault check, Timer2
         SETBIT(T2CONbits.TON); // Turn Timer 2 on
     }
@@ -39,12 +39,16 @@ void motor_init(uint8_t chan)
 
 void motor_set_duty(uint8_t chan, uint16_t duty_us)
 {
+    uint16_t duty = HIGH - duty_us * 1e-6 * 128e5 / 64;
+
     if (chan == CHANNEL_X)
     {
-        OC8RS = HIGH - duty_us; /* Load OCRS: next pwm duty cycle */
+        OC8R = duty;
+        OC8RS = duty; /* Load OCRS: next pwm duty cycle */
     }
     else if (chan == CHANNEL_Y)
     {
-        OC7RS = HIGH - duty_us; /* Load OCRS: next pwm duty cycle */
-    }  
+        OC7R = duty;
+        OC7RS = duty; /* Load OCRS: next pwm duty cycle */
+    }
 }
