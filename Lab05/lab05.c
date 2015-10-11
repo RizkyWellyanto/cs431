a63e95495a21e0b0152133f1dd8751e4415af549
Bo Liu <boliu1@illinois.edu>
2015-10-11 10:33:50 -0500

    Update lab05.c

--- Lab05/lab05.c
+++ Lab05/lab05.c
@@ -11,6 +11,7 @@
 #include "flexmotor.h"
 
 #define TIMER_INTERVEL 0x8fff
+#define LOOP_INTERVEL 2000
 
 /* Initial configuration by EE */
 // Primary (XT, HS, EC) Oscillator with PLL
@@ -72,7 +73,7 @@ void initCounterTimer3() { // for debouncing
     CLEARBIT(T3CONbits.TCS); // Select internal instruction cycle clock
     CLEARBIT(T1CONbits.TGATE); // Disable gated timer mode
 
-    TMR3 = 0x0000; // clear register
+    TMR3 = 0x00; // clear register
 
     SETBIT(T3CONbits.TON); // turn on Timer3
 }
@@ -83,49 +84,36 @@ int main(){
     lcd_initialize();
     lcd_clear();
 
-    initCounterTimer3();
+    initCounterTimer3(); // for deboucing
     initADCs();
 
-    motor_init(CHANNEL_X);
-    motor_init(CHANNEL_Y);
+    motor_init(CHANNEL_X); // using Timer2
+    motor_init(CHANNEL_Y); // using Timer2
 
     SETBIT(TRISEbits.TRISE8); // set trigger as task toggle
-    SETBIT(AD1PCFGHbits.PCFG20);
     SETBIT(TRISDbits.TRISD10); // set thumb as reset
-
-    SETBIT(TRISEbits.TRISE8); // set trigger as input
-    SETBIT(AD1PCFGHbits.PCFG20);
-    SETBIT(TRISDbits.TRISD10);
 
     uint16_t X;
     uint16_t Xmin = 0xffff;
     uint16_t Xmax = 0x0000;
 
     uint16_t Y;
-    uint16_t Ymin;
-    uint16_t Ymax;
+    uint16_t Ymin = 0xffff;
+    uint16_t Ymax = 0x0000;
     uint16_t Xpulse;
     uint16_t Ypulse;
 
     uint16_t loopCounter = 0;
 
-    Debouncer button1; // trigger
-    uint16_t button1Status;
+    Debouncer trigger;
+    uint16_t triggerStatus;
 
-    Debouncer button2; // thumb
-    uint16_t button2Status;
-
+    Debouncer thumb; // thumb
+    uint16_t thumbStatus;
 
     enum taskFlags {MEASURE_MAX_X, MEASURE_MIN_X, MEASURE_MAX_Y,
                     MEASURE_MIN_Y, SERVO_X, SERVO_Y}
     taskFlag = MEASURE_MAX_X;
-
-
-    if(loopCounter == 2000)
-    {
-        lcd_locate(5,0);
-        lcd_printf("%d", taskFlag);
-    }
 
     while(1)
     {
@@ -134,13 +122,13 @@ int main(){
         {
             TMR3 = 0x0000;
 
-            button_read(&button1, PORTEbits.RE8); // Joy-stick trigger button
-            button1Status = button_debounced(&button1);
-            if (button1Status != UNSTABLE && button1Status != UNCHANGED)
+            button_read(&trigger, PORTEbits.RE8); // Joy-stick trigger button
+            triggerStatus = button_debounced(&trigger);
+            if (triggerStatus != UNSTABLE && triggerStatus != UNCHANGED)
             {
-                if (button1Status == 0 )
+                if (triggerStatus == 0 )
                 {
-                    if (taskFlag++ >= SERVO_Y)
+                    if (taskFlag++ >= SERVO_Y) // Go to the next task
                     {
                         lcd_locate(0,7);
                         lcd_printf("All Done!");
@@ -148,19 +136,25 @@ int main(){
                 }
             }
 
-            button_read(&button2, PORTDbits.RD10); // Joy-stick thumb button
-            button2Status = button_debounced(&button2);
-            if (button2Status != UNSTABLE && button2Status != UNCHANGED)
+            button_read(&thumb, PORTDbits.RD10); // Joy-stick thumb button
+            thumbStatus = button_debounced(&thumb);
+            if (thumbStatus != UNSTABLE && thumbStatus != UNCHANGED)
             {
-                if (button2Status == 0 )
-                {
+                if (thumbStatus == 0 )
+                { // reset and start over again
+                    lcd_clear();
+                    uint16_t Xmin = 0xffff;
+                    uint16_t Xmax = 0x0000;
+                    uint16_t Ymin = 0xffff;
+                    uint16_t Ymax = 0x0000;
+                    loopCounter = 0;
                     taskFlag = MEASURE_MAX_X;
                 }
             }
         }
 
         // read X
-        if (loopCounter % 2000 == 0)
+        if (loopCounter % LOOP_INTERVEL == 0)
         {
             AD1CHS0bits.CH0SA = 0x004; //set ADC to Sample AN4 pin, Joystick X-axis
             SETBIT(AD1CON1bits.SAMP); //start to sample
@@ -170,7 +164,7 @@ int main(){
         }
 
         // read Y
-        if (loopCounter % 2000 == 0)
+        if (loopCounter % LOOP_INTERVEL == 0)
         {
             AD2CHS0bits.CH0SA = 0x005; //set ADC to Sample AN5 pin, Joystick Y-axis
             SETBIT(AD2CON1bits.SAMP); //start to sample
@@ -183,7 +177,7 @@ int main(){
         {
             case MEASURE_MAX_X :
 
-                if (loopCounter >= 2000)
+                if (loopCounter >= LOOP_INTERVEL)
                 {
                     loopCounter = 0;
                     lcd_locate(0,1);
@@ -194,7 +188,7 @@ int main(){
 
             case MEASURE_MIN_X :
 
-                if (loopCounter >= 2000)
+                if (loopCounter >= LOOP_INTERVEL)
                 {
                     loopCounter = 0;
                     lcd_locate(0,2);
@@ -206,7 +200,7 @@ int main(){
 
             case MEASURE_MAX_Y :      
 
-                if (loopCounter >= 2000)
+                if (loopCounter >= LOOP_INTERVEL)
                 {
                     loopCounter = 0;
                     lcd_locate(0,3);
@@ -217,7 +211,7 @@ int main(){
 
             case MEASURE_MIN_Y :
 
-                if (loopCounter >= 2000)
+                if (loopCounter >= LOOP_INTERVEL)
                 {
                     loopCounter = 0;
                     lcd_locate(0,4);
@@ -227,30 +221,30 @@ int main(){
                 break;
 
             case SERVO_X :
-                Xpulse = (X - Xmin) * 140L/(Xmax - Xmin) + 220;
+                Xpulse = (((X - Xmin) * 140L)/(Xmax - Xmin)) + 220;
                 
-                if (loopCounter >= 2000)
+                if (loopCounter >= LOOP_INTERVEL)
                 {
                     loopCounter = 0;
                     motor_set_duty(CHANNEL_X, Xpulse);
                     lcd_locate(0,5);
-                    lcd_printf("Xpwm:%d", X);
+                    lcd_printf("Xpwm:%d", Xpulse);
                 }
 
             case SERVO_Y :
-                Ypulse = (Y - Ymin) * 140L/(Ymax - Ymin) + 220;
+                Ypulse = (((Y - Ymin) * 140L)/(Ymax - Ymin)) + 220;
               
-                if (loopCounter >= 2000)
+                if (loopCounter >= LOOP_INTERVEL)
                 {
                     loopCounter = 0;
                     motor_set_duty(CHANNEL_Y, Ypulse);
                     lcd_locate(0,6);
-                    lcd_printf("Ypwm:%d", Y);
+                    lcd_printf("Ypwm:%d", Ypulse);
                 }
             default :
         	    break;
         }
     }
     
-    return 0;
+    return 0; 
 }
