7124b43a511a76f17ed152084d780eb980684a87
Bo Liu <boliu1@illinois.edu>
2015-10-11 15:11:43 -0500

    Update lab05.c

--- Lab05/lab05.c
+++ Lab05/lab05.c
@@ -26,31 +26,12 @@ _FWDT(FWDTEN_OFF);
 // Disable Code Protection
 _FGS(GCP_OFF);
 
-void initADCs(){
-    // ADC1 for X
-    CLEARBIT(AD1CON1bits.ADON);
-    //initialize PIN
-    SETBIT(TRISEbits.TRISE8); //set TRISE RE8 to input
-    CLEARBIT(AD1PCFGHbits.PCFG20); //set AD1 AN20 input pin as analog
-    //Configure AD1CON1
-    CLEARBIT(AD1CON1bits.AD12B); //set 10b Operation Mode
-    AD1CON1bits.FORM = 0; //set integer output
-    AD1CON1bits.SSRC = 0x7; //set automatic conversion
-    //Configure AD1CON2
-    AD1CON2 = 0; //not using scanning sampling
-    //Configure AD1CON3
-    CLEARBIT(AD1CON3bits.ADRC); //internal clock source
-    AD1CON3bits.SAMC = 0x1F; //sample-to-conversion clock = 31Tad
-    AD1CON3bits.ADCS = 0x2; //Tad = 3Tcy (Time cycles)
-    //Leave AD1CON4 at its default value
-    //enable ADC
-    SETBIT(AD1CON1bits.ADON);
-
-    // ADC2 for Y
+void initADC2(){
+    // ADC2 for Joystick axis
     CLEARBIT(AD2CON1bits.ADON);
 
-    SETBIT(TRISBbits.TRISB4);
-    CLEARBIT(AD2PCFGLbits.PCFG4);
+    SETBIT(TRISBbits.TRISB4); //
+    CLEARBIT(AD2PCFGLbits.PCFG4); // AD2 AN20 input pin set analog
     SETBIT(TRISBbits.TRISB5);
     CLEARBIT(AD2PCFGLbits.PCFG5);
     //Configure AD2CON1
@@ -67,30 +48,22 @@ void initADCs(){
     SETBIT(AD2CON1bits.ADON);
 }
 
-
-void initCounterTimer3() { // for debouncing
-    CLEARBIT(T3CONbits.TON); // Disable Timer
-    CLEARBIT(T3CONbits.TCS); // Select internal instruction cycle clock
-    CLEARBIT(T1CONbits.TGATE); // Disable gated timer mode
-
-    TMR3 = 0x00; // clear register
-
-    SETBIT(T3CONbits.TON); // turn on Timer3
-}
-
 int main(){
     //Init LCD
     __C30_UART=1;
     lcd_initialize();
     lcd_clear();
 
-    initCounterTimer3(); // for deboucing
-    initADCs();
+    lcd_locate(0,0);
+    lcd_printf("*Lab05*");
+
+    initADC2();
 
     motor_init(CHANNEL_X); // using Timer2
     motor_init(CHANNEL_Y); // using Timer2
 
     SETBIT(TRISEbits.TRISE8); // set trigger as task toggle
+    SETBIT(AD1PCFGHbits.PCFG20);
     SETBIT(TRISDbits.TRISD10); // set thumb as reset
 
     uint16_t X;
@@ -117,18 +90,18 @@ int main(){
 
     while(1)
     {
-        ++loopCounter;
-        if (TMR3 >= TIMER_INTERVEL) // debouncing period
-        {
-            TMR3 = 0x0000;
+        if (++loopCounter > LOOP_INTERVEL)
+            loopCounter = 0;
 
+        if (loopCounter >= LOOP_INTERVEL) // debouncing period
+        {
             button_read(&trigger, PORTEbits.RE8); // Joy-stick trigger button
             triggerStatus = button_debounced(&trigger);
             if (triggerStatus != UNSTABLE && triggerStatus != UNCHANGED)
             {
                 if (triggerStatus == 0 )
                 {
-                    if (taskFlag++ >= SERVO_Y) // Go to the next task
+                    if (++taskFlag > SERVO_Y) // Go to the next task
                     {
                         lcd_locate(0,7);
                         lcd_printf("All Done!");
@@ -143,10 +116,19 @@ int main(){
                 if (thumbStatus == 0 )
                 { // reset and start over again
                     lcd_clear();
-                    uint16_t Xmin = 0xffff;
-                    uint16_t Xmax = 0x0000;
-                    uint16_t Ymin = 0xffff;
-                    uint16_t Ymax = 0x0000;
+                    lcd_locate(0,0);
+                    lcd_printf("*Lab05*");
+                    initADC2();
+
+                    motor_init(CHANNEL_X); // using Timer2
+                    motor_init(CHANNEL_Y); // using Timer2
+
+                    Xmin = 0xffff;
+                    Xmax = 0x0000;
+
+                    Ymin = 0xffff;
+                    Ymax = 0x0000;
+
                     loopCounter = 0;
                     taskFlag = MEASURE_MAX_X;
                 }
@@ -154,23 +136,23 @@ int main(){
         }
 
         // read X
-        if (loopCounter % LOOP_INTERVEL == 0)
+        if (loopCounter % LOOP_INTERVEL/5 == 0)
         {
-            AD1CHS0bits.CH0SA = 0x004; //set ADC to Sample AN4 pin, Joystick X-axis
-            SETBIT(AD1CON1bits.SAMP); //start to sample
-            while(!AD1CON1bits.DONE); // wait for conversion to finish
-            CLEARBIT(AD1CON1bits.DONE); // MUST HAVE! clear conversion done bit
-            X = ADC1BUF0;
+            AD2CHS0bits.CH0SA = 0x004; //set ADC to Sample AN4 pin, Joystick X-axis
+            SETBIT(AD2CON1bits.SAMP); //start to sample
+            while(!AD2CON1bits.DONE); // wait for conversion to finish
+            CLEARBIT(AD2CON1bits.DONE); // MUST HAVE! clear conversion done bit
+            X = ADC2BUF0;
         }
 
         // read Y
-        if (loopCounter % LOOP_INTERVEL == 0)
+        if (loopCounter % LOOP_INTERVEL/5 == 0)
         {
             AD2CHS0bits.CH0SA = 0x005; //set ADC to Sample AN5 pin, Joystick Y-axis
             SETBIT(AD2CON1bits.SAMP); //start to sample
             while(!AD2CON1bits.DONE); // wait for conversion to finish
             CLEARBIT(AD2CON1bits.DONE); // MUST HAVE! clear conversion done bit
-            Y = ADC2BUF0; 
+            Y = ADC2BUF0;
         }
 
         switch (taskFlag)
@@ -179,10 +161,11 @@ int main(){
 
                 if (loopCounter >= LOOP_INTERVEL)
                 {
-                    loopCounter = 0;
                     lcd_locate(0,1);
                     lcd_printf("Xmax:%d   ", X);
                     Xmax = (Xmax > X) ? Xmax : X;
+                    //lcd_locate(10,1);
+                    //lcd_printf("X_max:%d   ", Xmax);
                 }
                 break;
 
@@ -190,22 +173,24 @@ int main(){
 
                 if (loopCounter >= LOOP_INTERVEL)
                 {
-                    loopCounter = 0;
                     lcd_locate(0,2);
                     lcd_printf("Xmin:%d   ", X);
                     Xmin = (Xmin < X) ? Xmin : X;
+                    //lcd_locate(10,2);
+                    //lcd_printf("X_min:%d   ", Xmin);
                 }
 
                 break;
 
-            case MEASURE_MAX_Y :      
+            case MEASURE_MAX_Y :
 
                 if (loopCounter >= LOOP_INTERVEL)
                 {
-                    loopCounter = 0;
                     lcd_locate(0,3);
                     lcd_printf("Ymax:%d   ", Y);
                     Ymax = (Ymax > Y) ? Ymax : Y;
+                    //lcd_locate(10,3);
+                    //lcd_printf("Y_max:%d   ", Ymax);
                 }
                 break;
 
@@ -213,38 +198,44 @@ int main(){
 
                 if (loopCounter >= LOOP_INTERVEL)
                 {
-                    loopCounter = 0;
                     lcd_locate(0,4);
                     lcd_printf("Ymin:%d   ", Y);
                     Ymin = (Ymin < Y) ? Ymin : Y;
+                    //lcd_locate(10,4);
+                    //lcd_printf("Y_min:%d   ", Ymin);
                 }
                 break;
 
             case SERVO_X :
-                Xpulse = (((X - Xmin) * 140L)/(Xmax - Xmin)) + 220;
-                
+
+                Xpulse = 2*240L*(X-Xmin)/(Xmax-Xmin); // HIGH - LOW = 240
+                // PERIOD - HIGH = 180
+                // the above magic numbers are for servo subtrim and throw adjustment
+                // Same for Y
+
                 if (loopCounter >= LOOP_INTERVEL)
                 {
-                    loopCounter = 0;
                     motor_set_duty(CHANNEL_X, Xpulse);
                     lcd_locate(0,5);
                     lcd_printf("Xpwm:%d", Xpulse);
                 }
+                break;
 
             case SERVO_Y :
-                Ypulse = (((Y - Ymin) * 140L)/(Ymax - Ymin)) + 220;
-              
+                Ypulse = 2*240L*(Y-Ymin)/(Ymax-Xmin);
+
                 if (loopCounter >= LOOP_INTERVEL)
                 {
-                    loopCounter = 0;
                     motor_set_duty(CHANNEL_Y, Ypulse);
                     lcd_locate(0,6);
                     lcd_printf("Ypwm:%d", Ypulse);
                 }
+                break;
+                
             default :
         	    break;
         }
     }
-    
-    return 0; 
+
+    return 0;
 }
