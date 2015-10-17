87213caab1a0a56ee80e041bafc335aaa6c9c39d
Bo Liu <boliu1@illinois.edu>
2015-10-17 12:43:26 -0500

    uses __delay_ms() instead of timer interrupt

--- Lab06/lab06.c
+++ Lab06/lab06.c
@@ -22,40 +22,6 @@ _FWDT(FWDTEN_OFF);
 // Disable Code Protection
 _FGS(GCP_OFF);
 
-uint16_t flag = 1;
-
-// Timer 1
-void init_timer1()
-{
-   CLEARBIT(T1CONbits.TON);   // Disable Timer
-   CLEARBIT(T1CONbits.TCS);   //Select internal instruction cycle clock (12.8 MHz)
-   CLEARBIT(T1CONbits.TGATE); //Disable Gated Timer mode
-   TMR1 = 0x00;               //Clear Timer Register
-   T1CONbits.TCKPS = 0b10;    //Set Prescaler (1:256)
-   PR1 = 2000;                //Set Period
-   IPC0bits.T1IP = 0x01;      //Set IPL
-   CLEARBIT(IEC0bits.T1IE);     //Enable INT
-   CLEARBIT(IFS0bits.T1IF);   //Clear IF
-   SETBIT(T1CONbits.TON);     //Enable Timer
-}
-
-// T1 interrupt every 10 ms.
-void
-__attribute__ (( __interrupt__, auto_psv)) _T1Interrupt(void)
-{
-   flag = 1;
-   CLEARBIT(IFS0bits.T1IF);
-}
-
-void wait()
-{
-    flag = 1;
-    init_timer1();
-    while(!flag); // spins
-    flag = 0;
-}
-
-
 int main(){
 	//Init LCD
 	__C30_UART=1;
@@ -68,50 +34,34 @@ int main(){
     const uint16_t NUM_SAMPLES = 5;
     uint16_t samples[NUM_SAMPLES];
     uint16_t i = 0;
-    uint16_t loopCounter = 0;
 
     init_adc1();
     touch_init();
-    init_timer1();
 
     while(1){
 
-        if(loopCounter++ >= 2000) {
-            loopCounter = 0;
-        }
-
-        wait();
-
         // sample X
-        //AD1CHS0bits.CH0SA = 0x00F; // X pin AN15
-        //touch_select_dim(DIM_X);
+        touch_select_dim(DIM_X);
 
         for (i = 0; i < NUM_SAMPLES; ++i) {
-            wait();
-            samples[i] = touch_read(DIM_X);
+            Delay_ms(2);
+            samples[i] = touch_adc();
         }
 
-        if(loopCounter == 2000) {
-            //lcd_clear_row(1);
-            lcd_locate(0, 1);
-            lcd_printf("X = %u        ", find_median(samples, 5));
-        }
-
-        wait();
+        
+        lcd_locate(0, 1);
+        lcd_printf("X = %u        ", find_median(samples, NUM_SAMPLES));
         
         // sample Y
-        //AD1CHS0bits.CH0SA = 0x009; // Y pin AN9
-        //touch_select_dim(DIM_Y);
+        touch_select_dim(DIM_Y);
 
         for (i = 0; i < NUM_SAMPLES; ++i) {
-            wait();
-            samples[i] = touch_read(DIM_Y);
+            Delay_ms(2);
+            samples[i] = touch_adc();
         }
 
-        if(loopCounter == 2000) {
-            lcd_locate(0, 2);
-            lcd_printf("Y = %u        ", find_median(samples, 5));
-        }
+        lcd_locate(0, 2);
+        lcd_printf("Y = %u        ", find_median(samples, NUM_SAMPLES));
     }
 
     return 0;
