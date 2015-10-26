b192f13b4a30bb5cd773e961711f4ee558224e23
Bo Liu <boliu1@illinois.edu>
2015-10-26 15:11:32 -0500

    add previous_delta

--- Lab07/pid_controller.c
+++ Lab07/pid_controller.c
@@ -9,7 +9,8 @@
 #include "pid_controller.h"
 
 void pid_controller_init(pid_controller_t * controller, float Target, float Delta_time, float Kp, float Ki, float Kd) {
-    controller->error = 0.0f;
+    controller->current_delta = 0.0f;
+    controller->previous_delta = 0.0f;
     controller->integral = 0.0f;
     controller->derivative = 0.0f;
     
@@ -31,34 +32,52 @@ void pid_controller_init(pid_controller_t * controller, float Target, float Delt
 
 uint16_t feed_back(pid_controller_t * controller, uint16_t pos) {
     // Proportional feedback
-    controller->error = controller->target - pos;
+    controller->current_delta = controller->target - pos;
     
-    // update controller history
-    controller->history[controller->history_idx] = controller->error;
-    
-    ++controller->history_idx;
-    if (controller->history_idx >= MEMORY)
-        controller->history_idx = 0;
-
-    int i = 0;
-    for(controller->integral = 0; i < MEMORY; ++i) {
-        // Integral feedback
-        controller->integral += controller->history[i];
-    }
+    // Integration feedback
+    controller->integral += controller->current_delta;
     
     // Derivative feedback
-    uint16_t previous = controller->error;
-    if(controller->history_idx == 0)
-        previous = controller->history[MEMORY];
-    else
-        previous = controller->history[controller->history_idx - 1];
-    
-    controller->derivative = (controller->error - controller->history[previous]) / controller->delta_time;
+    controller->derivative = (controller->current_delta - controller->previous_delta) / controller->delta_time;
+    controller->previous_delta = controller->current_delta;
      
-    float u = controller->kp*controller->error + controller->ki*controller->integral - controller->kd*controller->derivative;
+    float u = controller->kp * controller->current_delta + controller->ki * controller->integral - controller->kd * controller->derivative;
     
-
     // need to clamp u??
     
     return u + 290; // magic numbers
 }
+
+// uint16_t feed_back(pid_controller_t * controller, uint16_t pos) {
+//     // Proportional feedback
+//     controller->error = controller->target - pos;
+    
+//     // update controller history
+//     controller->history[controller->history_idx] = controller->error;
+    
+//     ++controller->history_idx;
+//     if (controller->history_idx >= MEMORY)
+//         controller->history_idx = 0;
+
+//     int i = 0;
+//     for(controller->integral = 0; i < MEMORY; ++i) {
+//         // Integral feedback
+//         controller->integral += controller->history[i];
+//     }
+    
+//     // Derivative feedback
+//     uint16_t previous = controller->error;
+//     if(controller->history_idx == 0)
+//         previous = controller->history[MEMORY];
+//     else
+//         previous = controller->history[controller->history_idx - 1];
+    
+//     controller->derivative = (controller->error - controller->history[previous]) / controller->delta_time;
+     
+//     float u = controller->kp*controller->error + controller->ki*controller->integral - controller->kd*controller->derivative;
+    
+
+//     // need to clamp u??
+    
+//     return u + 290; // magic numbers
+// }
