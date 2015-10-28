6a20fe40c35f60ae39edcf82bacb2f11e793cb3b
Bo Liu <boliu1@illinois.edu>
2015-10-27 21:33:36 -0500

    Update pid_controller.c

--- Lab07/pid_controller.c
+++ Lab07/pid_controller.c
@@ -11,15 +11,13 @@
 
 void pid_controller_init(pid_controller_t * controller, float Target, float Delta_time, float Kp, float Ki, float Kd) {
     controller->current_delta = 0.0f;
-    controller->previous_delta = 0.0f;
+    controller->previous_delta = 300.0f;
     controller->integral = 0.0f;
     controller->derivative = 0.0f;
 
     controller->delta_time = Delta_time;
 
     controller->target = Target;
-    
-    controller->feedback = 0.0f;
 
     controller->kp = Kp;
     controller->ki = Ki;
@@ -29,19 +27,17 @@ void pid_controller_init(pid_controller_t * controller, float Target, float Delt
 uint16_t feed_back(pid_controller_t * controller, uint16_t pos) {
     // Proportional feedback
     controller->current_delta = controller->target - pos;
-    
-    // this happens due to ball losing contact to flextouch temporarily
-    if( abs((controller->previous_delta - controller->current_delta)/controller->delta_time) > 10*controller->derivative )
-        controller->current_delta = controller->previous_delta;
+
+    // TODO: handling losing contact
 
     // Integration feedback
     controller->integral += controller->current_delta * controller->delta_time;
 
     // Derivative feedback
-    controller->derivative = (controller->previous_delta - controller->current_delta) / controller->delta_time;
+    controller->derivative = (controller->current_delta - controller->previous_delta) / controller->delta_time;
     controller->previous_delta = controller->current_delta;
 
-    controller->feedback = controller->kp * controller->current_delta + controller->ki * controller->integral - controller->kd * controller->derivative;
+    controller->feedback = controller->kp * controller->current_delta + controller->ki * controller->integral + controller->kd * controller->derivative;
 
-    return controller->feedback + 290; // 290 is the magic number to trim-center the midpoint of flextouch
+    return controller->feedback + 290; // magic numbers
 }
