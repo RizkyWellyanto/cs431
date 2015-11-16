04b28540b922c32e11c8878a8f7a477d157132d9
Bo Liu <boliu1@illinois.edu>
2015-11-16 13:17:32 -0600

    Create performance.h

---
+++ Lab09/performance.h
@@ -0,0 +1,19 @@
+#ifndef PERFORMANCE_H
+#define	PERFORMANCE_H
+
+#ifndef MOTOR_X_CHAN
+#define MOTOR_X_CHAN 1
+#endif
+#ifndef MOTOR_Y_CHAN
+#define MOTOR_Y_CHAN 2
+#endif
+
+#define W1 0.01    // 6 digits
+#define W2 0.01 // 6 digits
+#define DT 0.02  // 20ms
+
+void calcQEI(double Xset, double Xfilt, double Yset, double Yfilt);
+unsigned int getQEI();
+void setMotorDuty(uint8_t chan, uint16_t duty_us);
+
+#endif
